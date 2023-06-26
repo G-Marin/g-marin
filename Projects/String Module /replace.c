b2b40ca8ae5261e19f79ece72cb2d3c789f7ffc4
G-Marin <121909600+G-Marin@users.noreply.github.com>
2023-06-25 21:21:22 -0400

    Create replace.c

---
+++ Projects/String Module /replace.c
@@ -0,0 +1,113 @@
+/*--------------------------------------------------------------------*/
+/* replace.c                                                          */
+/* Author: Gabriel Marin                                                       */
+/*--------------------------------------------------------------------*/
+
+#include "str.h"
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+/*--------------------------------------------------------------------*/
+
+/* If pcFrom is the empty string, then write string pcLine to stdout
+   and return 0.  Otherwise write string pcLine to stdout with each
+   distinct occurrence of string pcFrom replaced with string pcTo,
+   and return a count of how many replacements were made.  Make no
+   assumptions about the maximum number of replacements or the
+   maximum number of characters in strings pcLine, pcFrom, or pcTo. */
+
+static size_t replaceAndWrite(const char *pcLine,
+                              const char *pcFrom, const char *pcTo)
+{
+
+   char *pointer = (char *)pcLine;
+   char *traversalPointer = (char *)pcLine; 
+   size_t found = 0;
+   size_t length = Str_getLength(pcFrom);
+   size_t counter = 0;
+
+   assert(pcLine != NULL);
+   assert(pcFrom != NULL);
+   assert(pcTo != NULL);
+
+   if(length == 0){
+      printf("%s", pcLine);
+      return 0;
+   }
+
+   while(*traversalPointer != '\0') {
+
+      if(found == 0){
+         pointer = Str_search(traversalPointer, pcFrom);
+         found = 1;
+      }
+
+      if(pointer == traversalPointer){
+
+         printf("%s", pcTo);
+         traversalPointer = pointer + length ;
+         pointer = pointer + length;
+         found = 0;
+         counter++;
+      }
+      
+      if(pointer != traversalPointer){
+      putchar(*traversalPointer);     
+      traversalPointer++;
+      }
+
+   }
+
+   return counter;  
+}
+
+   
+
+
+
+
+
+/*--------------------------------------------------------------------*/
+
+/* If argc is unequal to 3, then write an error message to stderr and
+   return EXIT_FAILURE.  Otherwise...
+   If argv[1] is the empty string, then write each line of stdin to
+   stdout, write a message to stderr indicating that 0 replacements
+   were made, and return 0.  Otherwise...
+   Write each line of stdin to stdout with each distinct occurrence of
+   argv[1] replaced with argv[2], write a message to stderr indicating
+   how many replacements were made, and return 0.
+   Assume that no line of stdin consists of more than MAX_LINE_SIZE-1
+   characters. */
+
+int main(int argc, char *argv[])
+{
+   enum {MAX_LINE_SIZE = 4096};
+   enum {PROPER_ARG_COUNT = 3};
+
+   char acLine[MAX_LINE_SIZE];
+   char *pcFrom;
+   char *pcTo;
+   size_t uReplaceCount = 0;
+   size_t temp;
+
+   if (argc != PROPER_ARG_COUNT)
+   {
+      fprintf(stderr, "usage: %s fromstring tostring\n", argv[0]);
+      return EXIT_FAILURE;
+   }
+
+   pcFrom = argv[1];
+   pcTo = argv[2];
+
+
+   while (fgets(acLine, MAX_LINE_SIZE, stdin) != NULL){
+      temp = replaceAndWrite(acLine, pcFrom, pcTo);
+      uReplaceCount += temp;
+   }
+
+   fprintf(stderr, "%lu replacements\n", (unsigned long)uReplaceCount);
+   return 0;
+}
